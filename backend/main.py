879b7554ce3bf6b0edeb7b05066aef9f88a0b178
Teemu Mäkelä <teemu.makela@cloud2.fi>
2026-02-06 10:17:53 +0200

    request.js_object

--- backend/main.py
+++ backend/main.py
@@ -102,7 +102,7 @@ else:
 
     class Default(WorkerEntrypoint):
         async def fetch(self, request):
-            return await asgi.fetch(app, request, self.env)
+            return await asgi.fetch(app, request.js_object, self.env)
             
 
             
